ef44a25f756719301ec374112219dbeb4f86c17d
Nady Zaretskaya <zaretskaya.nady@gmail.com>
2020-05-20 18:41:28 +0300

    Домашняя работа модуль 2 по Java

---
+++ HomeTaskQA/src/modul_02/Task01.java
@@ -0,0 +1,16 @@
+package modul_02;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		int x = 21;
+		int y = 7;
+		int a = x / y;
+			System.out.println("1) " + a);
+		float b = x % y;
+			System.out.println("2) " + b);
+		x *= x;
+			System.out.println("3) " + x);
+	}
+
+}
---
+++ HomeTaskQA/src/modul_02/Task06.java
@@ -0,0 +1,15 @@
+package modul_02;
+
+public class Task06 {
+	final static double height = 5.0;
+	final static double width = 15.0;
+
+	public static void main(String[] args) {
+		double perimeter = (height + width)*2;
+		double area = height * width;
+		System.out.println(perimeter);
+		System.out.println(area);
+
+	}
+
+}
